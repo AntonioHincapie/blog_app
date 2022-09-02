e807c300ab49b16bffd53be4c3f16b072a4c4dcc
AntonioHincapie <marcohincapiem@gmail.com>
2022-09-02 14:16:11 -0500

    fix migration

--- db/migrate/20220902184554_add_foreign_keys.rb
+++ db/migrate/20220902184554_add_foreign_keys.rb
@@ -1,9 +1,9 @@
 class AddForeignKeys < ActiveRecord::Migration[7.0]
   def change
-    add_foreign_key :posts, :users, column: :AuthorId, primary_key: :users_id
-    add_foreign_key :comments, :users, column: :AuthorId, primary_key: :users_id
-    add_foreign_key :likes, :users, column: :AuthorId, primary_key: :users_id
-    add_foreign_key :comments, :posts, column: :PostId, primary_key: :posts_id
-    add_foreign_key :likes, :posts, column: :PostId, primary_key: :posts_id
+    add_foreign_key :posts, :users, column: :AuthorId, primary_key: :id
+    add_foreign_key :comments, :users, column: :AuthorId, primary_key: :id
+    add_foreign_key :likes, :users, column: :AuthorId, primary_key: :id
+    add_foreign_key :comments, :posts, column: :PostId, primary_key: :id
+    add_foreign_key :likes, :posts, column: :PostId, primary_key: :id
   end
 end
