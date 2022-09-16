829ac8ed548c4af1874f61672e09da78b1e89b36
AntonioHincapie <marcohincapiem@gmail.com>
2022-09-16 17:01:48 -0500

    Modify posts controller and add view (new)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comments = Comment.where(post_id: params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    values = params.require(:post).permit(:title, :text)
+    @post = Post.new(author: current_user, title: values[:title], text: values[:text])
+
+    if @post.save
+      redirect_to users_path, notice: "Post has been published"
+    else
+      render :new
+    end
+  end
 end
