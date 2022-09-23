040b548d1a599c2fdcbb8d539e43461c0986476f
AntonioHincapie <marcohincapiem@gmail.com>
2022-09-23 13:13:09 -0500

    Fix models specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,45 +1,49 @@
 require 'rails_helper'
 
-RSpec.describe Post do
-  let(:user) { User.new(name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from EE.UU.') }
-  let(:post) { Post.new(author: user, title: 'title', text: 'text') }
+RSpec.describe Post, type: :model do
+  before do
+    @user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from EE.UU.')
+    @post = Post.new(author: @user, title: 'Title', text: 'Text')
+    Comment.create(author: @user, post: @post, text: 'Text1')
+    Comment.create(author: @user, post: @post, text: 'Text2')
 
-  before { post.save }
+    @user.save
+    @post.save
+  end
 
-  context 'When testing the Posts class' do
-    it 'should contain an author' do
-      expect(post.author).to eq(user)
+  context 'Testing the Posts class' do
+    it 'Should contain an author' do
+      expect(@post.author).to eq(@user)
     end
 
-    it 'should contain a title' do
-      expect(post.title).to eq('title')
+    it 'Should contain a title' do
+      expect(@post.title).to eq('Title')
     end
 
-    it 'should contain a text' do
-      expect(post.text).to eq('text')
+    it 'Should contain a text' do
+      expect(@post.text).to eq('Text')
     end
   end
 
-  context 'When testing the Posts methods' do
-    it 'should return the lenght of comments in comments_counter' do
-      expect(post.most_recent_comments.length).to eq(0)
+  context 'Testing Posts methods' do
+    it 'Should return the comments_counter' do
+      expect(@post.most_recent_comments.length).to eq(2)
     end
 
-    it 'should update post countersr' do
-      post.save
-      expect(user.post_counter).to eq(1)
+    it 'Should return the post countersr' do
+      expect(@user.post_counter).to eq(1)
     end
   end
 
-  context 'When testing Validations' do
-    it 'should validate that title exist' do
-      post.title = nil
-      expect(post).not_to be_valid
+  context 'Testing Validations' do
+    it 'Should validate that title exist' do
+      @post.title = nil
+      expect(@post).not_to be_valid
     end
 
-    it 'should validate that title is less than 250 characters lenght' do
-      post.title = 'a' * 251
-      expect(post).not_to be_valid
+    it 'should validate that title is less than 250 characters' do
+      @post.title = 'T' * 251
+      expect(@post).not_to be_valid
     end
   end
 end
