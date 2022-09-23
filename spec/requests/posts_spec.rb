78120e564e592603a3a8d26efaab444e1b5f0381
AntonioHincapie <marcohincapiem@gmail.com>
2022-09-23 13:32:08 -0500

    Fix request specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,38 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'Get page post #index' do
-    before(:each) do
-      get '/users/1/posts'
+    before(:all) do
+      @user = User.create(name: 'Antonio', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Colombia')
+      @post = Post.create(author: @user, title: 'My Post', text: 'This post')
+      get "/users/#{@user.id}/posts"
     end
 
-    it 'is a success' do
+    it 'Is a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'renders the correct template' do
-      expect(response).to render_template('index')
-    end
-
-    it 'renders and get the placeholder text' do
-      expect(response.body).to include 'Here'
+    it 'Renders and get the placeholder text' do
+      expect(response.body).to include 'Antonio'
     end
   end
 
   describe 'GET user page #show' do
-    before(:each) do
-      get '/users/1/posts/2'
+    before(:all) do
+      @user = User.create(name: 'Antonio', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Colombia')
+      @post = Post.create(author: @user, title: 'My Post', text: 'This post')
+      get "/users/#{@user.id}/posts/#{@post.id}"
     end
 
-    it 'is a success' do
+    it 'Is a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'renders the correct template' do
-      expect(response).to render_template('show')
-    end
-
     it 'renders and get the placeholder text' do
-      expect(response.body).to include 'Here'
+      expect(response.body).to include 'Antonio'
     end
   end
 end
