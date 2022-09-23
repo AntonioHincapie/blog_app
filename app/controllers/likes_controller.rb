5c9782e961b3b3f3f408a512b2cd389e2ad48e92
AntonioHincapie <marcohincapiem@gmail.com>
2022-09-23 16:57:32 -0500

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,12 +4,15 @@ class LikesController < ApplicationController
   def create
     likes_author = Like.where(author_id: current_user.id)
     likes_post = Like.where(post_id: params[:id])
+
     likes = likes_author.merge(likes_post)
+
     return unless likes == [] && current_user.id != Post.find(params[:id]).author_id
 
     like = Like.new
     like.author_id = current_user.id
     like.post_id = params[:id]
+    
     respond_to do |format|
       format.html do
         like.save
