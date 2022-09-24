a04b797b6864fb99c34e1e30617a56719225932e
AntonioHincapie <marcohincapiem@gmail.com>
2022-09-24 10:20:04 -0500

    Modify models and controllers - Add admin role

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,4 +36,11 @@ class PostsController < ApplicationController
       end
     end
   end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    authorize! :destroy, @post
+    @post.destroy
+    redirect_to(request.referer)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,8 @@ class Comment < ApplicationRecord
 
   after_save :update_comments_counter
 
+  after_destroy :update_comments_counter
+
   private
 
   def update_comments_counter
